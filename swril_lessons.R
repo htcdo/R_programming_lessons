60f605a76eda6a2b1e1bd71127f1af38542ec910
htcdo <helenatcdo@gmail.com>
2020-09-11 16:19:28 -0300

    lesson_7 matrices and data frames

--- swril_lessons.R
+++ swril_lessons.R
@@ -195,3 +195,47 @@ identical(vect, vect2)
 vect["bar"]
 
 vect[c("foo", "bar")]
+
+# lesson_7 matrices and data frames 
+
+my_vector <- 1:20
+
+my_vector
+
+dim(my_vector)
+
+length(my_vector)
+
+dim(my_vector) <- c(4, 5)
+
+dim(my_vector)
+
+attributes(my_vector)
+
+my_vector
+
+class(my_vector)
+
+my_matrix <- my_vector
+
+?matrix
+
+my_matrix2 <- matrix(data = 1:20, nrow = 4, ncol = 5, byrow = FALSE, dimnames = NULL)
+
+identical(my_matrix, my_matrix2)
+
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+
+cbind(patients, my_matrix)
+
+my_data <- data.frame(patients, my_matrix)
+
+my_data
+
+class(my_data)
+
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+
+colnames(my_data) <- cnames
+
+my_data
